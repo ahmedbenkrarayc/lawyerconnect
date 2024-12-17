29b08b0fd381e31700a759cfe429d7da2aa17fe0
ahmedbenkrarayc <ahmed.benkrara2@gmail.com>
2024-12-17 12:55:37 +0100

    project setup

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./*.{html,php}", "./*/*.{php,js}"],
+  theme: {
+    extend: {
+      screens: {
+        'sm': '0px',
+        'md': '768px',
+        'lg': '1024px',
+        'xl': '1280px'
+      },
+      fontFamily: {
+        poppins: ["Poppins", "serif"]
+      },
+    },
+  },
+  plugins: [],
+}
+
