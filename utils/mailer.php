7154804f1d9e33f492064eec2ddc6e8f77a6e943
ahmedbenkrarayc <ahmed.benkrara2@gmail.com>
2024-12-22 18:08:45 +0100

    fixed mailer function

--- utils/mailer.php
+++ utils/mailer.php
@@ -2,24 +2,24 @@
 use PHPMailer\PHPMailer\PHPMailer;
 use PHPMailer\PHPMailer\Exception;
 
-require __DIR__.'/../env.php';
 require __DIR__.'/../vendor/autoload.php';
+require __DIR__.'./../env.php';
 
-$mail = new PHPMailer(true);
 
 function sendMail($username, $to, $subject, $body){
+    $mail = new PHPMailer(true);
     try {
         //Server settings
         $mail->isSMTP();                                            
         $mail->Host       = 'smtp.gmail.com';                       
         $mail->SMTPAuth   = true;                                     
-        $mail->Username   = $mail_user;                 
-        $mail->Password   = $mail_password;                    
+        $mail->Username   = "ahmed.benkrara12@gmail.com";                 
+        $mail->Password   = "cgidqganvckpgtch";                    
         $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;           
         $mail->Port       = 587;                                      
     
         //Recipients
-        $mail->setFrom($mail_user, 'LawyerConnect');
+        $mail->setFrom("ahmed.benkrara12@gmail.com", 'LawyerConnect');
         $mail->addAddress($to, $username);
     
         // Content
