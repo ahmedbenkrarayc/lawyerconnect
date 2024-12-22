5f390680f425b70b0470ccf7dc9ddef903633f26
ahmedbenkrarayc <ahmed.benkrara2@gmail.com>
2024-12-22 18:40:16 +0100

    added submit lawyer unavailablity

--- views/lawyer/availability.php
+++ views/lawyer/availability.php
@@ -1,9 +1,28 @@
+<?php
+require './../../utils/db.php';
+require './../../guards/authGuard.php';
+
+if(!isAuth('lawyer')){
+  header('Location: ./../auth/login.php');
+}
+
+if($_SERVER['REQUEST_METHOD'] == 'POST'){
+  $sql = "INSERT INTO unavailable(id_lawyer, date_debut, date_fin) VALUES(?, ?, ?)";
+  $stmt = $conn->prepare($sql);
+  $stmt->bind_param('iss', $_COOKIE['user_id'], $_POST['start'], $_POST['end']);
+  if($stmt->execute()){
+    header('Location: '.$_SERVER['PHP_SELF']);
+  }
+}
+
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Availability</title>
     <link rel="stylesheet" href="./../../assets/css/output.css">
 </head>
 <body>
@@ -70,21 +89,18 @@
         </div>
         
         <div class="px-8 py-6">
-          <form class="space-y-6">
+          <form method="POST" action="<?php echo $_SERVER['PHP_SELF'] ?>" class="space-y-6">
             <div>
                 <label class="block text-sm font-medium text-gray-700 mb-1">Start Date</label>
-                <input type="date" class="px-4 py-2 w-full border border-gray-300 rounded-lg focus:ring-2 focus:ring-indigo-500 focus:border-transparent">
+                <input type="date" name="start" class="px-4 py-2 w-full border border-gray-300 rounded-lg focus:ring-2 focus:ring-indigo-500 focus:border-transparent">
             </div>
             <div>
                 <label class="block text-sm font-medium text-gray-700 mb-1">End Date</label>
-                <input type="date" class="px-4 py-2 w-full border border-gray-300 rounded-lg focus:ring-2 focus:ring-indigo-500 focus:border-transparent">
+                <input type="date" name="end" class="px-4 py-2 w-full border border-gray-300 rounded-lg focus:ring-2 focus:ring-indigo-500 focus:border-transparent">
             </div>
 
             <div class="border-t border-gray-200 pt-6">
               <div class="flex justify-end space-x-3">
-                <button type="button" class="px-4 py-2 text-sm font-medium text-gray-700 bg-white border border-gray-300 rounded-md shadow-sm hover:bg-gray-50">
-                  Cancel
-                </button>
                 <button type="submit" class="px-4 py-2 text-sm font-medium text-white bg-indigo-600 border border-transparent rounded-md shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
                   Set Unavailability
                 </button>
